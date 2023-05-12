31bfae3fbd52000d1c143c1f76e31cea4dce5989
Ytalo de Jesus Alves Correa Sousa <novoytalo@yahoo.com.br>
2023-05-12 16:57:23 -0300

    add basic osmdroid map as SlideshowFragment

--- app/src/main/java/com/example/gpstesty/ui/slideshow/SlideshowFragment.kt
+++ app/src/main/java/com/example/gpstesty/ui/slideshow/SlideshowFragment.kt
@@ -1,5 +1,7 @@
 package com.example.gpstesty.ui.slideshow
 
+
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,9 +10,15 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.gpstesty.databinding.FragmentSlideshowBinding
+import org.mapsforge.map.android.view.MapView
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.overlay.Marker
+
+import java.net.URL
 
 class SlideshowFragment : Fragment() {
-
+    private lateinit var mapView: MapView
     private var _binding: FragmentSlideshowBinding? = null
 
     // This property is only valid between onCreateView and
@@ -32,8 +40,25 @@ class SlideshowFragment : Fragment() {
         slideshowViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+        val mapViewM = binding.map
+        mapViewM.setTileSource(TileSourceFactory.OpenTopo)
+
+        val mapControler= mapViewM.controller
+        mapControler.setZoom(15.0)
+        val latitude = -1.4714765
+        val longitude = -48.4655708
+        val center = GeoPoint(latitude,longitude)
+        //Center start
+        mapControler.setCenter(center)
+        //Marker
+        val marker = Marker(mapViewM)
+        marker.position= center
+        marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_CENTER)
+        mapViewM.overlays.add(marker)
         return root
+
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
